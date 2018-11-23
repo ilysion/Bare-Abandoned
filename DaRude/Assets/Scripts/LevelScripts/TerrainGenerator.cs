7627e5aa617dc7d1892258ff8d9a9f009349e866
ilysion <Hannes.saariste@gmail.com>
2018-11-24 00:15:39 +0200

    Added map height generation with PerlinNoise and spawning trees.

---
+++ DaRude/Assets/Scripts/LevelScripts/TerrainGenerator.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TerrainGenerator : MonoBehaviour
+{
+
+    public int width = 1024;
+    public int height = 1024;
+    public int depth = 20;
+    public float scale = 20f;
+
+    public float offsetX = 1000f;
+    public float offsetY = 1000f;
+
+    public GameObject TreeMediumPrefab;
+    public GameObject TreeSmallPrefab;
+
+    private List<GameObject> TreesList;
+
+    void Start()
+    {
+        TreesList = new List<GameObject>();
+        // takes random offset so every time the map is different
+        offsetX = Random.Range(0f, 9999f);
+        offsetY = Random.Range(0f, 9999f);
+
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+        GenerateTrees(terrain.terrainData);
+        
+    }
+    
+
+    private void GenerateTrees(TerrainData terraindata)
+    {
+        Vector3 position = new Vector3(0, 0, 0);
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int PlaceRandom1 = Random.Range(1, 400);
+                int PlaceRandom2 = Random.Range(1, 200);
+                if (PlaceRandom1 < 2)
+                {
+                    position.x = x;
+                    position.z = y;
+                    position.y = GetHeightDetail(terraindata, Mathf.RoundToInt(position.z), Mathf.RoundToInt(position.x));
+                    GameObject mytree = GameObject.Instantiate(TreeSmallPrefab);
+                    mytree.transform.position = position;
+                    TreesList.Add(mytree);           
+                }
+
+                if (PlaceRandom2 < 2)
+                {
+                    position.x = x;
+                    position.z = y;
+                    position.y = GetHeightDetail(terraindata, Mathf.RoundToInt(position.z), Mathf.RoundToInt(position.x));
+                    GameObject mytree = GameObject.Instantiate(TreeMediumPrefab);
+                    mytree.transform.position = position;
+                    TreesList.Add(mytree);
+                }
+            }
+
+        }
+        
+        
+    }
+    
+    private float GetHeightDetail(TerrainData terrainData, int x, int y)
+    {
+        float y_01 = (float)y / (float)terrainData.detailHeight;
+        float x_01 = (float)x / (float)terrainData.detailWidth;
+        //Gets height at this coordinates
+        float height = terrainData.GetHeight(Mathf.RoundToInt(y_01 * terrainData.heightmapWidth), Mathf.RoundToInt(x_01 * terrainData.heightmapWidth));
+        return height;
+    }
+    
+
+    //Generates Terrain land
+    TerrainData GenerateTerrain(TerrainData terraindata)
+    {
+        terraindata.heightmapResolution = width + 1;
+        terraindata.size = new Vector3(width, depth, height);
+        terraindata.SetHeights(0, 0, GenerateHeights());
+        return terraindata;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+
+        }
+        return heights;
+    }
+
+    float CalculateHeight(int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
--- DaRude/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ DaRude/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
+    public Terrain terrain;
+    public int PlayerStartingHeight = 10;
+
     public UIController Canvas;
     public int Health;
     private bool OutOfDeathCircle;
@@ -23,6 +26,10 @@ public class PlayerController : MonoBehaviour {
         curTimeStamp = Time.time;
         sceneRestartDelay = 4;
 
+        //Sets character starting position according to generated map
+        Vector3 currentpos = transform.position;
+        currentpos.y = GetHeightDetail(terrain.terrainData, Mathf.RoundToInt(currentpos.x), Mathf.RoundToInt(currentpos.z)) + PlayerStartingHeight;
+        transform.position = currentpos;
     }
 	
 	// Update is called once per frame
@@ -75,5 +82,14 @@ public class PlayerController : MonoBehaviour {
             //hit small three
             
         }
+    }
+
+    private float GetHeightDetail(TerrainData terrainData, int x, int y)
+    {
+        float y_01 = (float)y / (float)terrainData.detailHeight;
+        float x_01 = (float)x / (float)terrainData.detailWidth;
+        //Gets height at this coordinates
+        float height = terrainData.GetHeight(Mathf.RoundToInt(y_01 * terrainData.heightmapWidth), Mathf.RoundToInt(x_01 * terrainData.heightmapWidth));
+        return height;
     }
 }
